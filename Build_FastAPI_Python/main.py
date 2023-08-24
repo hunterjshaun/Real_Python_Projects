6702007cdd0a9c654d15489844a2b0bfa5dadc9f
josh <josh@josh.com>
2023-08-24 09:53:02 -0400

    commit

--- Build_FastAPI_Python/main.py
+++ Build_FastAPI_Python/main.py
@@ -1,10 +1,11 @@
 # main.py
-
+# Import FastAPI for creating the API 
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() # Create the app object to handle requests and responses
 
-
+# Create a route to the root endpoint to run the function below and return a JSON response.
+# The function below will return a JSON response with a message key and a value of "Hello World"
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
